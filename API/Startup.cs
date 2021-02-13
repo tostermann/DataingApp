fc0b35a01442aae4e3b3726b1ae9f7565d97670b
tostermann <Troy_Ostermann@hotmail.com>
2021-02-13 13:01:40 -0600

    End of Section 5:  Client login and register

--- API/Startup.cs
+++ API/Startup.cs
@@ -52,8 +52,7 @@ namespace API
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
-                app.UseSwagger();
-                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "API v1"));
+
             }
 
             app.UseHttpsRedirection();
