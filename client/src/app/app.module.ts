a098da69e3af35e4b68ee74b37887f7f90b9d802
tostermann <Troy_Ostermann@hotmail.com>
2021-02-14 19:26:46 -0600

    End of section 6: Angular Routing

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -12,6 +12,12 @@ import { NavComponent } from './nav/nav.component';
 import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
 import { HomeComponent } from './home/home.component';
 import { RegisterComponent } from './register/register.component';
+import { MemberListComponent } from './members/member-list/member-list.component';
+import { MemberDetailComponent } from './members/member-detail/member-detail.component';
+import { ListsComponent } from './lists/lists.component';
+import { MessagesComponent } from './messages/messages.component';
+import {ToastrModule} from 'ngx-toastr'
+import { SharedModule } from './_modules/shared.module';
 
 
 @NgModule({
@@ -19,7 +25,11 @@ import { RegisterComponent } from './register/register.component';
     AppComponent,
     NavComponent,
     HomeComponent,
-    RegisterComponent
+    RegisterComponent,
+    MemberListComponent,
+    MemberDetailComponent,
+    ListsComponent,
+    MessagesComponent
   ],
   imports: [
     BrowserModule,
@@ -28,8 +38,7 @@ import { RegisterComponent } from './register/register.component';
     NgbModule,
     FormsModule,
     BrowserAnimationsModule,
-    BsDropdownModule.forRoot()
-    
+    SharedModule
   ],
   providers: [],
   bootstrap: [AppComponent]
